0ec26a972d8ca8b26f18fbd33040edd3310e74f5
Marco Abreu <falecomabreu@gmail.com>
2021-09-01 22:05:41 -0300

    Primeira versão do lwcOne

--- Zumbicidio/force-app/main/default/lwc/lwcOne/lwcOne.js
+++ Zumbicidio/force-app/main/default/lwc/lwcOne/lwcOne.js
@@ -1,10 +1,24 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api, wire } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
+
+const FIELDS = ["Bunker__c.Name", "Bunker__c.Ativo__c"];
 
 export default class LwcOne extends LightningElement {
-  something = "world";
+  @api recordId;
+
+  @wire(getRecord, { recordId: "$recordId", fields: FIELDS }) // vai buscar os dados no background
+  bunker; // Variavel que recebe os dados
+
+  something = "world"; // atribuição direta
 
   changeHandler(event) {
     // eslint-disable-next-line no-alert
     alert(event.target.value);
+  }
+
+  // Usado no HTML para exibir o valor
+  get name() {
+    console.log(this.bunker);
+    return this.bunker.data.fields.Name.value;
   }
 }
