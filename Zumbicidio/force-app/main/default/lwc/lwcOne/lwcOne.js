72b5619016b12b3f3ee117993ab6096e4c116b37
Marco Abreu <falecomabreu@gmail.com>
2021-09-01 22:38:42 -0300

    revisão do código

--- Zumbicidio/force-app/main/default/lwc/lwcOne/lwcOne.js
+++ Zumbicidio/force-app/main/default/lwc/lwcOne/lwcOne.js
@@ -1,15 +1,21 @@
 import { LightningElement, api, wire } from "lwc";
 import { getRecord } from "lightning/uiRecordApi";
+// import { getRecord, getFieldValue } from "lightning/uiRecordApi";
+
+// import NAME_FIELD from '@salesforce/schema/Bunker__c.Name';
+// import ATIVO_FIELD from '@salesforce/schema/Bunker__c.Ativo__c';
 
 const FIELDS = ["Bunker__c.Name", "Bunker__c.Ativo__c"];
+// const FIELDS = [NAME_FIELD, ATIVO_FIELD];
 
 export default class LwcOne extends LightningElement {
   @api recordId;
 
   @wire(getRecord, { recordId: "$recordId", fields: FIELDS }) // vai buscar os dados no background
+  // @wire(getRecord, { recordId: '$recordId', fields: FIELDS }) // vai buscar os dados no background
   bunker; // Variavel que recebe os dados
 
-  something = "world"; // atribuição direta
+  something = "Hello World!"; // atribuição direta
 
   changeHandler(event) {
     // eslint-disable-next-line no-alert
@@ -20,5 +26,6 @@ export default class LwcOne extends LightningElement {
   get name() {
     console.log(this.bunker);
     return this.bunker.data.fields.Name.value;
+    // return getFieldValue(this.bunker.data, NAME_FIELD);
   }
 }
